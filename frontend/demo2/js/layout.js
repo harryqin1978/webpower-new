cbfc83d4f90e8a47468ce7ab454382a039ec027d
Harry Qin <zergling9999@hotmail.com>
2014-07-03 15:27:10 +0800

    right sidebar toggle

--- frontend/demo2/js/layout.js
+++ frontend/demo2/js/layout.js
@@ -1,9 +1,24 @@
 jQuery(document).ready(function($) {
 
+  /* configuration */
+  layout_body_minwidth = 1024;
   layout_left_width = 200;
-  layout_right_width = 200;
   layout_header_height = 63;
   layou_footer_height = 63;
+  layout_main_inner_minwidth = 800;
+  layout_main_inner_border_width = 1;
+  layout_main_inner_margin = 10;
+  layoyt_animate_speed = 500;
+  layout_right_target_width = 0;
+
+  /* start */
+
+  layout_main_inner_outside = layout_main_inner_border_width + layout_main_inner_margin;
+
+  $('#main-inner')
+    .css('min-width', layout_main_inner_minwidth+'px')
+    .css('border-width', layout_main_inner_border_width+'px')
+    .css('margin', layout_main_inner_margin+'px')
 
   $( window ).resize(function() {
     rebuildLayout();
@@ -11,14 +26,34 @@ jQuery(document).ready(function($) {
 
   rebuildLayout();
 
+  $('#show-small-sidebar').click(function(event) {
+    toggleSidebar(300);
+  });
+
+  $('#show-big-sidebar').click(function(event) {
+    toggleSidebar(550);
+  });
+
+  $('#hide-sidebar').click(function(event) {
+    toggleSidebar(0);
+  });
+
 });
 
 function rebuildLayout() {
-  window_width = $( window ).width();
-  window_height = $( window ).height();
+  var window_width = $( window ).width();
+  var window_height = $( window ).height();
+  if (window_width < layout_body_minwidth) {
+    container_width = layout_body_minwidth;
+  } else {
+    container_width = window_width;
+  }
+
+  var main_inner_width = container_width-layout_left_width-layout_right_target_width-layout_main_inner_outside*2;
+
   $('#header')
     .css('position', 'absolute')
-    .css('width', '100%')
+    .css('width', container_width+'px')
     .css('height', layout_header_height+'px')
     .css('top', '0')
     .css('left', '0');
@@ -30,20 +65,31 @@ function rebuildLayout() {
     .css('left', '0');
   $('#right')
     .css('position', 'absolute')
-    .css('width', layout_right_width+'px')
+    .css('width', layout_right_target_width+'px')
     .css('height', (window_height-layout_header_height-layou_footer_height)+'px')
     .css('top', layout_header_height+'px')
-    .css('right', '0');
+    .css('left', (container_width-layout_right_target_width)+'px');
   $('#main')
     .css('position', 'absolute')
-    .css('width', (window_width-layout_left_width-layout_right_width)+'px')
+    .css('width', (container_width-layout_left_width)+'px')
     .css('height', (window_height-layout_header_height-layou_footer_height)+'px')
     .css('top', layout_header_height+'px')
     .css('left', layout_left_width+'px');
+  $('#main-inner')
+    .css('width', main_inner_width)
+    .css('height', (window_height-layout_header_height-layou_footer_height-layout_main_inner_outside*2)+'px');
   $('#footer')
     .css('position', 'absolute')
-    .css('width', (window_width-layout_left_width)+'px')
+    .css('width', (container_width-layout_left_width)+'px')
     .css('height', layou_footer_height+'px')
     .css('top', (window_height-layou_footer_height)+'px')
     .css('left', layout_left_width+'px');
+}
+
+function toggleSidebar(target_width) {
+  layout_right_target_width = target_width;
+  $('#right').animate({width: layout_right_target_width+'px', left: (container_width-layout_right_target_width)+'px'}, layoyt_animate_speed);
+
+  var main_inner_width = container_width-layout_left_width-layout_right_target_width-layout_main_inner_outside*2;
+  $('#main-inner').animate({width: main_inner_width+'px'});
 }
